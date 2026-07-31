8a2c3b0a0369c01b309b2b37d2c3744ffcaa4963
vldeyycom <vldeyycom@gmail.com>
2026-07-31 07:31:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://s.shopee.co.id/2LVluJcClu", "_blank");
+    window.open("https://s.shopee.co.id/4VbcNgFv7b", "_blank");
 });
 
 
